308ab4e0683bcde970b33f4efbb40e805f47853c
Nfinzki <francescodiluzio07@gmail.com>
2022-06-05 11:41:23 +0200

    Refactoring

--- app/src/main/java/it/di/unipi/sam/noisyscanner/RecordingFragment.java
+++ app/src/main/java/it/di/unipi/sam/noisyscanner/RecordingFragment.java
@@ -3,22 +3,20 @@ package it.di.unipi.sam.noisyscanner;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
-public class ButtonFragment extends Fragment implements View.OnClickListener {
+public class RecordingFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_button, container, false);
+        return inflater.inflate(R.layout.fragment_recording, container, false);
     }
 
     @Override
@@ -29,7 +27,6 @@ public class ButtonFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
-        Snackbar.make(view, "Prova", Snackbar.LENGTH_SHORT).show();
         FloatingActionButton button = (FloatingActionButton) view;
 
         if (button.getTag().equals("rec")) {
