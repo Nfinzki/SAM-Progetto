81c804f1b805c9af629c35711a81ebf5946bd3cf
Nfinzki <francescodiluzio07@gmail.com>
2022-06-05 13:28:26 +0200

    Added last recordings UI

---
+++ app/src/main/java/it/di/unipi/sam/noisyscanner/RecordingAdapter.java
@@ -0,0 +1,49 @@
+package it.di.unipi.sam.noisyscanner;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.card.MaterialCardView;
+
+public class RecordingAdapter extends RecyclerView.Adapter<RecordingAdapter.RecordViewHolder> {
+    @NonNull
+    @Override
+    public RecordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.last_recording_item, parent, false);
+        return new RecordViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecordViewHolder holder, int position) {
+        //TODO Implementare
+        holder.city.setText("Prova");
+        holder.timestamp.setText("13:20");
+        holder.decibel.setText("82 Db");
+    }
+
+    @Override
+    public int getItemCount() {
+        return 20; //TODO Modificare
+    }
+
+    public static class RecordViewHolder extends RecyclerView.ViewHolder {
+        MaterialCardView card;
+        TextView city;
+        TextView timestamp;
+        TextView decibel;
+
+        RecordViewHolder(View itemView) {
+            super(itemView);
+
+            card = (MaterialCardView) itemView.findViewById(R.id.recording_item);
+            city = (TextView) itemView.findViewById(R.id.city);
+            timestamp = (TextView) itemView.findViewById(R.id.timestamp);
+            decibel = (TextView) itemView.findViewById(R.id.decibel);
+        }
+    }
+}
--- app/src/main/java/it/di/unipi/sam/noisyscanner/RecordingFragment.java
+++ app/src/main/java/it/di/unipi/sam/noisyscanner/RecordingFragment.java
@@ -3,6 +3,8 @@ package it.di.unipi.sam.noisyscanner;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +25,11 @@ public class RecordingFragment extends Fragment implements View.OnClickListener 
     public void onViewCreated(View view, Bundle savedInstanceState) {
         FloatingActionButton button = (FloatingActionButton) view.findViewById(R.id.micButton);
         button.setOnClickListener(this);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recent_recordings);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(new RecordingAdapter());
     }
 
     @Override
