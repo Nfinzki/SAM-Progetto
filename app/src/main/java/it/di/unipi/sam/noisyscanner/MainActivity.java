927ea0fbc3fc3fac44c6cc143869311e1fe4e75d
Nfinzki <francescodiluzio07@gmail.com>
2022-06-05 11:40:56 +0200

    Added chip navbar

--- app/src/main/java/it/di/unipi/sam/noisyscanner/MainActivity.java
+++ app/src/main/java/it/di/unipi/sam/noisyscanner/MainActivity.java
@@ -2,24 +2,82 @@ package it.di.unipi.sam.noisyscanner;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ChipNavigationBar.OnItemSelectedListener {
+    private ChipNavigationBar chipNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.placeholder, new ButtonFragment());
-        ft.commit();
+        chipNavigationBar = findViewById(R.id.navigation_bar);
+
+        if (savedInstanceState == null) {
+            Log.d("ON_CREATE", "savedInstanceState = null");
+
+            chipNavigationBar.setItemSelected(R.id.recording, true);
+
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.placeholder, new RecordingFragment())
+                .commit();
+        } else {
+            Log.d("ON_CREATE", "savedInstanceState = " + savedInstanceState.get("ChipNavBar"));
+
+            chipNavigationBar.setItemSelected(savedInstanceState.getInt("ChipNavBar"), true);
+
+            Fragment fragment = null;
+            switch (savedInstanceState.getInt("ChipNavBar")) {
+                case R.id.recording:
+                    fragment = new RecordingFragment();
+                    break;
+                case R.id.statistics:
+                    fragment = new StatisticFragment();
+                    break;
+            }
+
+            if (fragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.placeholder, fragment)
+                        .commit();
+            }
+        }
+
+        chipNavigationBar.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("ChipNavBar", chipNavigationBar.getSelectedItemId());
+        Log.d("SAVE_STATE", "Saved: " + chipNavigationBar.getSelectedItemId());
+    }
+
+    @Override
+    public void onItemSelected(int i) {
+        Fragment fragment = null;
+
+        switch (i) {
+            case R.id.recording:
+                fragment = new RecordingFragment();
+                break;
+
+            case R.id.statistics:
+                fragment = new StatisticFragment();
+                break;
+        }
+
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.placeholder, fragment)
+                    .commit();
+        }
     }
 }
