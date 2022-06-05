fbc25b8d1a354e711bd07f9fe8dd8c17a19b81ee
Nfinzki <francescodiluzio07@gmail.com>
2022-06-05 11:41:42 +0200

    Added StatisticFragment

---
+++ app/src/main/java/it/di/unipi/sam/noisyscanner/StatisticFragment.java
@@ -0,0 +1,24 @@
+package it.di.unipi.sam.noisyscanner;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link StatisticFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class StatisticFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_statistic, container, false);
+    }
+}
