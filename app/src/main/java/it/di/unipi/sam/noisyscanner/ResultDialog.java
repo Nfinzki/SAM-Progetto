4530b313ff8ed04903cafc2d59c87454b82e7eb2
Nfinzki <francescodiluzio07@gmail.com>
2022-06-13 10:35:03 +0200

    Added custom dialog

---
+++ app/src/main/java/it/di/unipi/sam/noisyscanner/ResultDialog.java
@@ -0,0 +1,20 @@
+package it.di.unipi.sam.noisyscanner;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class ResultDialog extends DialogFragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.result_dialog, container, false);
+        getDialog().setTitle("Dialog di prova");
+        return v;
+    }
+}
