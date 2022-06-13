dc598790f4e2ca43653c2196d391614a099b785c
Nfinzki <francescodiluzio07@gmail.com>
2022-06-13 10:33:50 +0200

    Added startService

--- app/src/main/java/it/di/unipi/sam/noisyscanner/RecordingFragment.java
+++ app/src/main/java/it/di/unipi/sam/noisyscanner/RecordingFragment.java
@@ -1,12 +1,18 @@
 package it.di.unipi.sam.noisyscanner;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,17 +36,21 @@ public class RecordingFragment extends Fragment implements View.OnClickListener 
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recent_recordings);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
-        recyclerView.addItemDecoration(new DividerItemDecoration(view.getContext(), DividerItemDecoration.VERTICAL));
+        //recyclerView.addItemDecoration(new DividerItemDecoration(view.getContext(), DividerItemDecoration.VERTICAL));
         recyclerView.setAdapter(new RecordingAdapter());
     }
 
     @Override
     public void onClick(View view) {
         FloatingActionButton button = (FloatingActionButton) view;
+        Context context = view.getContext();
 
         if (button.getTag().equals("rec")) {
             button.setImageResource(R.drawable.ic_stop);
             button.setTag("stop");
+
+            Intent i = new Intent(context, RecordingService.class);
+            context.startService(i);
         } else {
             button.setImageResource(R.drawable.ic_microphone);
             button.setTag("rec");
