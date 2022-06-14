86a0e132f7b1acbab19aeede435a11b86b860e1b
Nfinzki <francescodiluzio07@gmail.com>
2022-06-14 18:22:40 +0200

    Bug fixing

--- app/src/main/java/it/di/unipi/sam/noisyscanner/database/RecordingDAO.java
+++ app/src/main/java/it/di/unipi/sam/noisyscanner/database/RecordingDAO.java
@@ -1,15 +1,13 @@
 package it.di.unipi.sam.noisyscanner.database;
 
 import androidx.room.Dao;
-import androidx.room.Insert;
 import androidx.room.Query;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Dao
 public interface RecordingDAO {
-    @Query("SELECT * FROM Recording LIMIT :limit")
+    @Query("SELECT * FROM Recording ORDER BY timestamp DESC LIMIT :limit")
     List<Recording> getRecentRecordings(int limit);
 
     @Query("SELECT city FROM Recording " +
@@ -19,7 +17,7 @@ public interface RecordingDAO {
     @Query("SELECT strftime('%m', timestamp) AS month FROM Recording GROUP BY month HAVING MAX(Recording.decibel)")
     String getLoudestMonth();
 
-    @Query("SELECT strftime('%d', timestamp) as day FROM Recording GROUP BY day HAVING MAX(Recording.decibel)")
+    @Query("SELECT strftime('%d-%m-%Y', timestamp) as day FROM Recording GROUP BY day HAVING MAX(Recording.decibel)")
     String getLoudestDay();
 
     @Query("SELECT strftime('%H', timestamp) as hour FROM Recording GROUP BY hour HAVING MAX(Recording.decibel)")
