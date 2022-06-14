5bf6d7cf3beda24978331128de86bcb895ce5b9c
Nfinzki <francescodiluzio07@gmail.com>
2022-06-14 18:26:40 +0200

    Implemented almost all the recording logic

--- app/src/main/java/it/di/unipi/sam/noisyscanner/RecordingJob.java
+++ app/src/main/java/it/di/unipi/sam/noisyscanner/RecordingJob.java
@@ -1,18 +1,38 @@
 package it.di.unipi.sam.noisyscanner;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.media.MediaRecorder;
 import android.util.Log;
 
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+
 import java.io.IOException;
+import java.util.List;
+
+import it.di.unipi.sam.noisyscanner.database.AppDatabase;
 
 public class RecordingJob implements Runnable {
     private double decibelSum = 0;
     private int numSum = 0;
     private final Context context;
+    private final FusedLocationProviderClient fusedLocationClient;
+    private final Geocoder geocoder;
+    private final AppDatabase db;
+    private final RecordingService.OnNewDataListener newDataListener;
 
-    public RecordingJob(Context context) {
+    public RecordingJob(Context context, FusedLocationProviderClient fusedLocationClient, Geocoder geocoder, RecordingService.OnNewDataListener dataListener) {
         this.context = context;
+        this.fusedLocationClient = fusedLocationClient;
+        this.geocoder = geocoder;
+        newDataListener = dataListener;
+
+        db = AppDatabase.getDatabaseInstance(context);
     }
 
     @Override
@@ -30,7 +50,7 @@ public class RecordingJob implements Runnable {
             e.printStackTrace();
         }
 
-        while(!Thread.currentThread().isInterrupted()) {
+        while (!Thread.currentThread().isInterrupted()) {
             int maxAmplitude = mediaRecorder.getMaxAmplitude();
             if (maxAmplitude != 0) {
                 decibelSum += getDecibel(maxAmplitude);
@@ -61,9 +81,49 @@ public class RecordingJob implements Runnable {
         mediaRecorder.release();
         mediaRecorder = null;
 
-        double avgDecibels = decibelSum / numSum;
-        Log.d("Db AVG", avgDecibels + "");
-        //TODO Insert into DB
+        double avgDecibels = 0;
+        if (decibelSum != 0) {
+            avgDecibels = decibelSum / numSum;
+            Log.d("Db AVG", avgDecibels + "");
+            avgDecibels = (double) Math.round(avgDecibels * 100d) / 100d;
+            Log.d("Db AVG rounded", avgDecibels + "");
+        } else {
+            //TODO Show Dialog che non è stato rilevato nulla
+        }
+
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            db.recordingDAO().insertRecording(avgDecibels, (String)context.getText(R.string.location_not_available));
+        } else {
+            double finalAvgDecibels = avgDecibels;
+
+            fusedLocationClient.getLastLocation().addOnSuccessListener(location -> {
+                String registeredLocation = null;
+                Log.d("LOC", location.toString());
+
+                List<Address> addressList = null;
+                if (geocoder != null) {
+                    try {
+                        addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (addressList != null && addressList.size() > 0) {
+                        Address address = addressList.get(0);
+                        Log.d("GEO", address.getLocality());
+                        registeredLocation = address.getLocality();
+                    }
+                } else {
+                    registeredLocation = location.getLatitude() + " " + location.getLongitude();
+                }
+
+                String finalRegisteredLocation = registeredLocation;
+                new Thread(() -> db.recordingDAO().insertRecording(finalAvgDecibels, finalRegisteredLocation)
+                ).start();
+
+                newDataListener.onNewData(finalAvgDecibels, "12-06-2022 12:20:22", finalRegisteredLocation); //TODO Aggiungere il timestamp
+            });
+        }
     }
 
     private double getDecibel(int maxAmplitude) {
