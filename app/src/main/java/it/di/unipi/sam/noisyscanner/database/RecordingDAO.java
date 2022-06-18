8f554e6d0ecd198f3f9abfa3af96fa794295119b
Nfinzki <francescodiluzio07@gmail.com>
2022-06-18 21:45:52 +0200

    Added queries

--- app/src/main/java/it/di/unipi/sam/noisyscanner/database/RecordingDAO.java
+++ app/src/main/java/it/di/unipi/sam/noisyscanner/database/RecordingDAO.java
@@ -10,23 +10,33 @@ public interface RecordingDAO {
     @Query("SELECT * FROM Recording ORDER BY timestamp DESC LIMIT :limit")
     List<Recording> getRecentRecordings(int limit);
 
-    @Query("SELECT city AS value, max(decibel) AS maxDecibel FROM Recording")
+    @Query("SELECT city AS value, max(decibel) AS decibel FROM Recording")
     Result getLoudestCity();
 
-    @Query("SELECT strftime('%m', timestamp) AS value, max(decibel) AS maxDecibel FROM Recording")
+    @Query("SELECT strftime('%m', timestamp) AS value, max(decibel) AS decibel FROM Recording")
     Result getLoudestMonth();
 
-    @Query("SELECT strftime('%d-%m-%Y', timestamp) AS value, MAX(decibel) AS maxDecibel FROM Recording")
+    @Query("SELECT strftime('%d-%m-%Y', timestamp) AS value, MAX(decibel) AS decibel FROM Recording")
     Result getLoudestDay();
 
-    @Query("SELECT strftime('%H', timestamp) AS value, MAX(decibel) AS maxDecibel FROM Recording")
+    @Query("SELECT strftime('%H', timestamp) AS value, MAX(decibel) AS decibel FROM Recording")
     Result getLoudestHour();
 
     @Query("INSERT INTO Recording (decibel, city) VALUES (:decibel, :city)")
     void insertRecording(double decibel, String city);
 
+    @Query("SELECT strftime('%m', timestamp) AS value, AVG(decibel) AS decibel FROM Recording " +
+            "WHERE strftime('%Y', timestamp) = :year GROUP BY value")
+    List<Result> getAvgPerMonth(String year);
+
+    @Query("SELECT MIN(strftime('%Y', timestamp)) FROM Recording")
+    String getMinYear();
+
+    @Query("SELECT MAX(strftime('%Y', timestamp)) FROM Recording")
+    String getMaxYear();
+
     static class Result {
         public String value;
-        public double maxDecibel;
+        public double decibel;
     }
 }
